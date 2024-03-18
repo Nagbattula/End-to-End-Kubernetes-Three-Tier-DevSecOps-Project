32aacdbb9ff80009159a80bb1d47d5b17f893bdb
Nagbattula <battula.nagaraj2@gmail.com>
2024-03-18 10:51:21 +0530

    changed statelock configuration

--- Jenkins-Server-TF/backend.tf
+++ Jenkins-Server-TF/backend.tf
@@ -4,7 +4,7 @@ terraform {
     region         = "us-east-1"
     key            = "Jenkins-Server-TF/terraform.tfstate"
     dynamodb_table = "Lock-Files"
-    encrypt        = true
+    encrypt        = false
   }
   required_version = ">=0.13.0"
   required_providers {
